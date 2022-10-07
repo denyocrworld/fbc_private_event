9f8303db960e24176c9e593455522ae1d9edb366
magerngulik <magerngulik@gmail.com>
2022-10-07 17:31:48 +0700

    menambahkan cart pertama

--- lib/module/zulkarnaim/zulkarnaim_dashboard/view/zulkarnaim_dashboard_view.dart
+++ lib/module/zulkarnaim/zulkarnaim_dashboard/view/zulkarnaim_dashboard_view.dart
@@ -16,8 +16,183 @@ class ZulkarnaimDashboardView extends StatefulWidget {
         child: Padding(
           padding: const EdgeInsets.all(20.0),
           child: Column(
-            children: const [
-              //body
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                child: Row(
+                  children: [
+                    Stack(
+                      children: [
+                        Container(
+                          height: 250.0,
+                          width: 250,
+                          decoration: const BoxDecoration(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(
+                                16.0,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          left: 0,
+                          bottom: 30,
+                          child: Container(
+                            height: 200,
+                            width: 200,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: const BorderRadius.all(
+                                Radius.circular(
+                                  24.0,
+                                ),
+                              ),
+                              gradient: LinearGradient(
+                                begin: Alignment.topRight,
+                                end: Alignment.bottomLeft,
+                                colors: [
+                                  Colors.white,
+                                  Colors.grey[200]!,
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          bottom: 0,
+                          left: 0,
+                          child: Row(
+                            children: [
+                              Container(
+                                height: 50.0,
+                                width: 50.0,
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: const BorderRadius.only(
+                                    bottomLeft: Radius.circular(
+                                      30,
+                                    ),
+                                  ),
+                                  gradient: LinearGradient(
+                                    begin: Alignment.bottomRight,
+                                    end: Alignment.topLeft,
+                                    colors: [
+                                      Colors.white,
+                                      Colors.grey[300]!,
+                                    ],
+                                  ),
+                                ),
+                                child: const Icon(
+                                  Icons.remove,
+                                  size: 28.0,
+                                ),
+                              ),
+                              Container(
+                                height: 50.0,
+                                width: 50.0,
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  gradient: LinearGradient(
+                                    begin: Alignment.topLeft,
+                                    end: Alignment.bottomRight,
+                                    colors: [
+                                      Colors.grey[100]!,
+                                      Colors.white,
+                                    ],
+                                  ),
+                                ),
+                                child: const Center(
+                                  child: Text(
+                                    "4",
+                                    style: TextStyle(
+                                      fontSize: 28.0,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                height: 50.0,
+                                width: 50.0,
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: const BorderRadius.only(
+                                    topRight: Radius.circular(
+                                      30,
+                                    ),
+                                  ),
+                                  gradient: LinearGradient(
+                                    begin: Alignment.topLeft,
+                                    end: Alignment.bottomRight,
+                                    colors: [
+                                      Colors.white,
+                                      Colors.grey[300]!,
+                                    ],
+                                  ),
+                                ),
+                                child: const Icon(
+                                  Icons.add,
+                                  size: 28.0,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Positioned(
+                          right: 30,
+                          top: 0,
+                          child: Container(
+                            height: 190,
+                            width: 190,
+                            decoration: const BoxDecoration(
+                              image: DecorationImage(
+                                image: NetworkImage(
+                                  "https://i.ibb.co/ChG42vq/vegetable-banner.png",
+                                ),
+                                fit: BoxFit.cover,
+                              ),
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(
+                                  16.0,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text(
+                          "Rp. 15.000",
+                          style: TextStyle(
+                            fontSize: 35.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.green,
+                          ),
+                        ),
+                        Text(
+                          "Mie Ayam Spesial",
+                          style: TextStyle(
+                            fontSize: 21.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                        ),
+                        Text(
+                          "3000 kcal",
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
@@ -26,5 +201,6 @@ class ZulkarnaimDashboardView extends StatefulWidget {
   }
 
   @override
-  State<ZulkarnaimDashboardView> createState() => ZulkarnaimDashboardController();
+  State<ZulkarnaimDashboardView> createState() =>
+      ZulkarnaimDashboardController();
 }
