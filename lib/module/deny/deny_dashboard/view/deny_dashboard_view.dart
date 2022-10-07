05b9e778be1a1c1e0a3960c9cc2867d8062a6092
Deny <flutterlabz@gmail.com>
2022-10-07 08:34:36 +0700

    .

--- lib/module/deny/deny_dashboard/view/deny_dashboard_view.dart
+++ lib/module/deny/deny_dashboard/view/deny_dashboard_view.dart
@@ -14,103 +14,210 @@ class DenyDashboardView extends StatefulWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: Wrap(
-          runSpacing: 20.0,
-          spacing: 20.0,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            //body
-            Card(
-              shape: ContinuousRectangleBorder(
-                borderRadius: BorderRadius.circular(64.0),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  children: const [
-                    Text("Contoh card"),
-                  ],
-                ),
-              ),
-            ),
             Container(
-              width: 100.0,
-              height: 100.0,
+              width: 300.0,
               decoration: const BoxDecoration(
-                image: DecorationImage(
-                  image: NetworkImage(
-                    "https://i.ibb.co/3pPYd14/freeban.jpg",
-                  ),
-                  fit: BoxFit.cover,
-                ),
+                color: Colors.white,
                 borderRadius: BorderRadius.all(
                   Radius.circular(
                     16.0,
                   ),
                 ),
-              ),
-            ),
-            Container(
-              width: 200,
-              height: 120.0,
-              margin: const EdgeInsets.only(right: 10.0),
-              decoration: BoxDecoration(
-                image: const DecorationImage(
-                  image: NetworkImage(
-                    "https://images.unsplash.com/photo-1482049016688-2d3e1b311543?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=710&q=80",
-                  ),
-                  fit: BoxFit.cover,
-                ),
-                borderRadius: const BorderRadius.all(
-                  Radius.circular(16.0),
-                ),
-                color: Colors.blue[400],
-              ),
-              child: Stack(
-                children: [
-                  Container(
-                    padding: const EdgeInsets.all(6.0),
-                    margin: const EdgeInsets.all(8.0),
-                    decoration: BoxDecoration(
-                      color: Colors.green[800],
-                      borderRadius: const BorderRadius.all(
-                        Radius.circular(
-                          12.0,
-                        ),
-                      ),
-                    ),
-                    child: const Text(
-                      "PROMO",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 8.0,
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    bottom: 0,
-                    left: 0,
-                    right: 0,
-                    child: Container(
-                      padding: const EdgeInsets.all(12.0),
-                      decoration: const BoxDecoration(
-                        color: Colors.black38,
-                        borderRadius: BorderRadius.only(
-                          bottomLeft: Radius.circular(16.0),
-                          bottomRight: Radius.circular(16.0),
-                        ),
-                      ),
-                      child: const Text(
-                        "Avocado and Eeg Toast",
-                        overflow: TextOverflow.ellipsis,
-                        style: TextStyle(
-                          fontSize: 11.0,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Color(0x19000000),
+                    blurRadius: 24,
+                    offset: Offset(0, 11),
                   ),
                 ],
               ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Column(
+                  children: [
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: 160.0,
+                      decoration: const BoxDecoration(
+                        image: DecorationImage(
+                          image: NetworkImage(
+                            "https://i.ibb.co/3pPYd14/freeban.jpg",
+                          ),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Column(
+                        children: [
+                          Row(
+                            children: const [
+                              Text(
+                                "PRODUCTIVITY",
+                                style: TextStyle(
+                                  fontSize: 12.0,
+                                ),
+                              ),
+                              Spacer(),
+                              Text(
+                                "3 days ago",
+                                style: TextStyle(
+                                  fontSize: 10.0,
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 10.0,
+                          ),
+                          const Text(
+                            "7 Skills of Highly Effective Programmers",
+                            style: TextStyle(
+                              fontSize: 16.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 6.0,
+                          ),
+                          const Text(
+                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                            maxLines: 4,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                              fontSize: 12.0,
+                            ),
+                          ),
+                          ListTile(
+                            contentPadding: const EdgeInsets.all(0.0),
+                            leading: CircleAvatar(
+                              backgroundColor: Colors.grey[200],
+                              backgroundImage: const NetworkImage(
+                                "https://i.ibb.co/QrTHd59/woman.jpg",
+                              ),
+                            ),
+                            title: const Text("Jessica Doe"),
+                            trailing: const Text(
+                              "Read more",
+                              style: TextStyle(
+                                fontSize: 10.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            Wrap(
+              runSpacing: 20.0,
+              spacing: 20.0,
+              children: [
+                //body
+                Card(
+                  shape: ContinuousRectangleBorder(
+                    borderRadius: BorderRadius.circular(64.0),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: const [
+                        Text("Contoh card"),
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 100.0,
+                  height: 100.0,
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: NetworkImage(
+                        "https://i.ibb.co/3pPYd14/freeban.jpg",
+                      ),
+                      fit: BoxFit.cover,
+                    ),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(
+                        16.0,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 200,
+                  height: 120.0,
+                  margin: const EdgeInsets.only(right: 10.0),
+                  decoration: BoxDecoration(
+                    image: const DecorationImage(
+                      image: NetworkImage(
+                        "https://images.unsplash.com/photo-1482049016688-2d3e1b311543?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=710&q=80",
+                      ),
+                      fit: BoxFit.cover,
+                    ),
+                    borderRadius: const BorderRadius.all(
+                      Radius.circular(16.0),
+                    ),
+                    color: Colors.blue[400],
+                  ),
+                  child: Stack(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.all(6.0),
+                        margin: const EdgeInsets.all(8.0),
+                        decoration: BoxDecoration(
+                          color: Colors.green[800],
+                          borderRadius: const BorderRadius.all(
+                            Radius.circular(
+                              12.0,
+                            ),
+                          ),
+                        ),
+                        child: const Text(
+                          "PROMO",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 8.0,
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        bottom: 0,
+                        left: 0,
+                        right: 0,
+                        child: Container(
+                          padding: const EdgeInsets.all(12.0),
+                          decoration: const BoxDecoration(
+                            color: Colors.black38,
+                            borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.circular(16.0),
+                              bottomRight: Radius.circular(16.0),
+                            ),
+                          ),
+                          child: const Text(
+                            "Avocado and Eeg Toast",
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                              fontSize: 11.0,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
           ],
         ),
