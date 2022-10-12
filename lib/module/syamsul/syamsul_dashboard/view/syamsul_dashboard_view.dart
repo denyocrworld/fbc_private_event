c5a48bcc6385eeb68032a44a0a7e305554556961
msyamsul725 <msyamsul7279@gmail.com>
2022-10-12 19:45:57 +0700

    .

--- lib/module/syamsul/syamsul_dashboard/view/syamsul_dashboard_view.dart
+++ lib/module/syamsul/syamsul_dashboard/view/syamsul_dashboard_view.dart
@@ -22,8 +22,8 @@ class SyamsulDashboardView extends StatefulWidget {
               Container(
                 width: 360.0,
                 height: 360.0,
-                decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.all(
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(
                     Radius.circular(
                       16.0,
                     ),
@@ -74,7 +74,7 @@ class SyamsulDashboardView extends StatefulWidget {
                                       const SizedBox(
                                         height: 20.0,
                                       ),
-                                      Container(
+                                      const SizedBox(
                                         height: 80.0,
                                         width: 120.0,
                                         child: Text(
@@ -93,7 +93,7 @@ class SyamsulDashboardView extends StatefulWidget {
                                         margin: const EdgeInsets.only(
                                           right: 30.0,
                                         ),
-                                        child: Text(
+                                        child: const Text(
                                           "\$ 30.00  ",
                                           textAlign: TextAlign.right,
                                           style: TextStyle(
@@ -112,8 +112,8 @@ class SyamsulDashboardView extends StatefulWidget {
                                         child: Row(
                                           mainAxisAlignment:
                                               MainAxisAlignment.spaceBetween,
-                                          children: [
-                                            const Icon(
+                                          children: const [
+                                            Icon(
                                               Icons.star,
                                               size: 24.0,
                                               color: Colors.orange,
@@ -157,8 +157,8 @@ class SyamsulDashboardView extends StatefulWidget {
                       ),
                       width: 220,
                       height: 380.0,
-                      decoration: BoxDecoration(
-                        borderRadius: const BorderRadius.all(
+                      decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.all(
                           Radius.circular(
                             16.0,
                           ),
@@ -177,6 +177,165 @@ class SyamsulDashboardView extends StatefulWidget {
                       ),
                     ),
                   ],
+                ),
+              ),
+              Container(
+                width: 160.0,
+                height: 220.0,
+                decoration: const BoxDecoration(
+                  color: Color(0xff54805A),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(
+                      16.0,
+                    ),
+                  ),
+                ),
+                child: Stack(
+                  children: [
+                    Container(
+                      width: 100.0,
+                      height: 140.0,
+                      margin: const EdgeInsets.only(left: 80.0, top: 60),
+                      child: Image.network(
+                        "https://i.ibb.co/7NJjsvN/a.png",
+                        width: 64.0,
+                        height: 64.0,
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(
+                        left: 20.0,
+                        top: 20.0,
+                      ),
+                      padding: const EdgeInsets.symmetric(vertical: 4.0),
+                      width: 90.0,
+                      child: const Text(
+                        "Montela Deliciosa",
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18.0,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 40.0,
+                      height: 40.0,
+                      margin: const EdgeInsets.only(
+                        top: 160,
+                        left: 20.0,
+                      ),
+                      child: const CircleAvatar(
+                        backgroundColor: Colors.white24,
+                        radius: 12.0,
+                        child: Icon(
+                          Icons.add,
+                          color: Colors.white,
+                          size: 24.0,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              Container(
+                width: 320.0,
+                height: 120.0,
+                decoration: const BoxDecoration(
+                  color: Color(0xff54805A),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(
+                      16.0,
+                    ),
+                  ),
+                ),
+                child: SizedBox(
+                  height: 100.0,
+                  child: Row(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.only(
+                          left: 20.0,
+                        ),
+                        height: 100.0,
+                        width: 80.0,
+                        child: Image.network(
+                          "https://i.ibb.co/7NJjsvN/a.png",
+                          width: 52.0,
+                          height: 52.0,
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20.0,
+                      ),
+                      SizedBox(
+                        height: 120.0,
+                        width: 100.0,
+                        child: Container(
+                          margin: const EdgeInsets.only(
+                            bottom: 20.0,
+                            left: 10.0,
+                          ),
+                          width: MediaQuery.of(context).size.width,
+                          child: Column(
+                            children: [
+                              const SizedBox(
+                                height: 28.0,
+                              ),
+                              const Text(
+                                "Montella Deliciosa",
+                                textAlign: TextAlign.left,
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18.0,
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 10.0,
+                              ),
+                              Container(
+                                margin: const EdgeInsets.only(
+                                  right: 40.0,
+                                ),
+                                child: const Text(
+                                  "\$ 20.0",
+                                  textAlign: TextAlign.start,
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 12.0,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(
+                          left: 40.0,
+                        ),
+                        width: 40.0,
+                        height: 40.0,
+                        child: const CircleAvatar(
+                          backgroundColor: Colors.white24,
+                          radius: 12.0,
+                          child: Icon(
+                            Icons.add,
+                            color: Colors.white,
+                            size: 24.0,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ],
